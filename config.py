754598a6555b7866706bed1a527a215013746c23
JuanDavidHernandez2021 <jhdavidjuan@gmail.com>
2025-09-25 11:36:19 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://jdavid:jdavidc@isladigital:3311/f58_jdavid')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql+pymysql://jdavid:jdavidc@isladigital.xyz:3311/f58_jdavid')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your_unique_and_secret_key')  # Clave secreta para sesiones
     # Google OAuth2 credentials (set these env vars to enable Google login locally)
