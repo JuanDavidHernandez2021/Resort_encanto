29d3ae41412cd9c5324c1776180005fc3a1ae58e
JuanDavidHernandez2021 <jhdavidjuan@gmail.com>
2025-10-16 11:38:51 -0500

    Update perfil_usuario_routes.py

--- routes/usuario/perfil_usuario_routes.py
+++ routes/usuario/perfil_usuario_routes.py
@@ -5,31 +5,31 @@ from models.baseDatos import Usuario
 perfil_usuario_bp = Blueprint('perfil_usuario_bp', __name__, template_folder='../templates')
 
 # Ruta del perfil de usuario
-@perfil_usuario_bp.route("/perfil_usuario")
-def perfil_usuario():
+#@perfil_usuario_bp.route("/perfil_usuario")
+#def perfil_usuario():
     # Si no hay sesión activa, redirige al login
-    if 'user' not in session:
-        flash("Por favor inicia sesión primero.", "warning")
-        return redirect(url_for('main.demo_login'))  # o la ruta de tu login real
+    #if 'user' not in session:
+     #   flash("Por favor inicia sesión primero.", "warning")
+      #  return redirect(url_for('main.demo_login'))  # o la ruta de tu login real
 
     # Obtener los datos de la sesión
-    user_data = session['user']
+    #user_data = session['user']
 
     # Buscar el usuario real en la base de datos (por ID)
-    usuario_db = Usuario.query.filter_by(idUsuario=user_data['id']).first()
+    #usuario_db = Usuario.query.filter_by(idUsuario=user_data['id']).first()
 
-    if usuario_db:
-        usuario = {
-            'nombre': usuario_db.usuario,
-            'correo': usuario_db.correo,
-            'rol': getattr(usuario_db, 'rol', 'Usuario estándar')
-        }
-    else:
+    #if usuario_db:
+      #  usuario = {
+       #     'nombre': usuario_db.usuario,
+        #    'correo': usuario_db.correo,
+         #   'rol': getattr(usuario_db, 'rol', 'Usuario estándar')
+        #}
+    #else:
         # Si no lo encuentra, usa los datos de sesión
-        usuario = {
-            'nombre': user_data.get('nombre', ''),
-            'correo': user_data.get('correo', ''),
-            'rol': user_data.get('rol', 'Usuario estándar')
-        }
+     #   usuario = {
+      #      'nombre': user_data.get('nombre', ''),
+       #     'correo': user_data.get('correo', ''),
+        #    'rol': user_data.get('rol', 'Usuario estándar')
+        #}
 
-    return render_template("usuario/perfil_usuario.html", usuario=usuario)
+    #return render_template("usuario/perfil_usuario.html", usuario=usuario)
